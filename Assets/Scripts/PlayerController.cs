4d25b9c3fff52acbbd376325f166dd16cca9deda
RVCC-sample-student <crosbie.git@gmail.com>
2020-04-15 12:37:53 -0400

    Add UI Count text to PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class PlayerController : MonoBehaviour
 {
-
+    // variables for physics movement
     public float speed;
-
     private Rigidbody rb;
 
+    //variables for counting and displaying count
+    private int count;
+    public Text countTextUI;
+
+
+
+    // called once before the first update cycle of the game
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetUIText();
     }
+
 
     void FixedUpdate()
     {
@@ -26,13 +36,23 @@ public class PlayerController : MonoBehaviour
     }
 
 
+    // called when the player object collider enters a trigger volume
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetUIText();
         }
     }
+
+    // Move common UI text update to function
+    private void SetUIText()
+    {
+        countTextUI.text = "Count: " + count.ToString();
+    }
+
 
 }
 
