a8311aac59be40a044642f43f576fb526b888d9e
RVCC-sample-student <crosbie.git@gmail.com>
2020-04-15 15:59:30 -0400

    Add WebGL Build

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject objectToFollow;
+    private Vector3 offset;   // distance between objectToFollow and camera
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - objectToFollow.transform.position;
+    }
+
+    // LateUpdate is called once per frame after all other updates
+    void LateUpdate()
+    {
+        transform.position = objectToFollow.transform.position + offset;
+    }
+}
